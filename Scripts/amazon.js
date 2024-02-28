fb400ddba774de3d477ccb8d749c5999f2ff4d7d
Vishal Sain <vishal.sain2@gmail.com>
2024-02-28 18:15:01 +0530

    make the cart quantity interactive

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -66,12 +66,22 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         });
 
         if(matchingItem){
-            matchingItem+=1;
+            matchingItem.quantity+=1;
         }else{
         cart.push({
             productId : productId,
             quantity : 1
         });
-    }
+        }
+
+        let cartQuantity = 0;
+
+        cart.forEach((item)=>{
+            cartQuantity += item.quantity;
+        })
+        console.log(cartQuantity);
+        console.log(cart);
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
